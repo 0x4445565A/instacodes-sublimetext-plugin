20cd38de0e842c74edc4046c6c789572eab4e9bb
Bradon Martinez <mbrandonweb@gmail.com>
2013-11-13 12:18:24 -0800

    Adding first version of the plugin with only selected text support.

---
+++ instacodes.py
@@ -0,0 +1,19 @@
+import sublime, sublime_plugin, re, urllib
+from webbrowser import open
+
+def sendToBrowser(code, syntax):
+  webbrowser.open('http://InstaCod.es/?post_code=' + code + '&post_lang=' + syntax)
+
+class SelectionToInstacodeCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    sel = self.view.sel()[0]
+    if not sel.empty():
+      selection = urllib.parse.quote_plus(self.view.substr(sel))
+      syntax = re.search('\/.*\/', self.view.settings().get('syntax')).group(0).replace('/', '')
+      sendToBrowser(selection, syntax)
+    else:
+      print('Nothing was Selected')
+
+#class FileToInstacodeCommand(sublime_plugin.TextCommand):
+#  def run(self, edit):
+#    print('we need to do stuff here')

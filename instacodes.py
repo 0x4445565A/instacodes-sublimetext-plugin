d1f63afdf015f4de156ef1a384226b4767d62c1f
Bradon Martinez <mbrandonweb@gmail.com>
2013-11-13 16:34:29 -0800

    Added File->Post To Instaco.de support

--- instacodes.py
+++ instacodes.py
@@ -10,8 +10,11 @@ class SelectionToInstacodeCommand(sublime_plugin.TextCommand):
     else:
       print('Nothing was Selected')
 
+class FileToInstacodeCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    selection = urllib.parse.quote_plus(self.view.substr(self.view.visible_region()))
+    syntax = re.search('\/.*\/', self.view.settings().get('syntax')).group(0).replace('/', '')
+    sendToBrowser(selection, syntax)
+
 def sendToBrowser(code, syntax):
   webbrowser.open('http://InstaCod.es/?post_code=' + code + '&post_lang=' + syntax)
-#class FileToInstacodeCommand(sublime_plugin.TextCommand):
-#  def run(self, edit):
-#    print('we need to do stuff here')
